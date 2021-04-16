1a7dc1c73f2a9e22ebed3babce526fe1edfbc78b
吴品龙 <2892571690@qq.com>
2021-04-16 17:25:03 +0800

    配置了反向代理

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 /**
  * index.js
  * api地址管理
+ * 第四位参数：当前的请求是否需要loading
  */
 import request from './request';
 
-export const reqLogin = (data) => request('/login', data, 'POST')
+export const reqLogin = (data) => request('/login', data, 'POST', false)
