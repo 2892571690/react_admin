eac694029b5bcfe5ecece1ba2eabcc2c495bd3a9
吴品龙 <2892571690@qq.com>
2021-04-19 14:15:31 +0800

    侧边栏完成

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import store from "../../redux/store";
-import { Redirect } from "react-router";
+import { Redirect, Switch, Route } from "react-router-dom";
 import { Layout } from "antd";
 import NavLeft from '../../components/nav-left/NavLeft'
 import Header from '../../components/header/Header'
-
+import Home from '../Home/Home'
+import Category from '../Category/Category'
+import Product from '../Product/Product'
+import Role from '../Role/Role'
+import User from '../User/User'
+import Bar from '../Charts/Bar'
+import Line from '../Charts/Line'
+import Pie from '../Charts/Pie'
 const {Footer, Sider, Content } = Layout;
 export default class Admin extends Component {
   render() {
@@ -19,7 +26,19 @@ export default class Admin extends Component {
         </Sider>
         <Layout>
           <Header>Header</Header>
-          <Content>Content</Content>
+          <Content>
+            <Switch>
+              <Route path='/home' component={Home}/>
+              <Route path='/category' component={Category}/>
+              <Route path='/product' component={Product}/>
+              <Route path='/role' component={Role}/>
+              <Route path='/user' component={User}/>
+              <Route path='/bar' component={Bar}/>
+              <Route path='/line' component={Line}/>
+              <Route path='/pie' component={Pie}/>
+              <Redirect to='/home'/>
+            </Switch>
+          </Content>
           <Footer style={{background:'#ccc',textAlign:'center'}}>推荐使用谷歌浏览器，可以获得更佳的页面操作体验!</Footer>
         </Layout>
       </Layout>
